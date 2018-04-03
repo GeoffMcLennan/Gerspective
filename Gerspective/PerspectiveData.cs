5e9b4116829e861e2f7a9fcb40f05f7517aa7c94
Geoff McLennan <geoff.d.mclennan@gmail.com>
2018-04-02 21:43:15 -0700

    INITIAL COMMIT

---
+++ Gerspective/PerspectiveData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerspective
+{
+    class PerspectiveData
+    {
+        public double[,] XLeft;
+        public double[,] XRight;
+        public double[,] Lines;
+    }
+}
